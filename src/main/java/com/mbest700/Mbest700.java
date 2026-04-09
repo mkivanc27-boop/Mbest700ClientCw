c8f820306915ab939c8452d436fc176f8f5e4d90
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-09 18:07:22 +0300

    Update Mbest700.java

--- src/main/java/com/mbest700/Mbest700.java
+++ src/main/java/com/mbest700/Mbest700.java
@@ -1,11 +1,13 @@
 package com.mbest700;
 
 import com.mbest700.modules.CombatModules;
+import com.mbest700.modules.MovementModules;
 import net.fabricmc.api.ClientModInitializer;
 
 public class Mbest700 implements ClientModInitializer {
     public static CombatModules combat = new CombatModules();
+    public static MovementModules movement = new MovementModules();
+
     @Override
     public void onInitializeClient() {}
 }
-
