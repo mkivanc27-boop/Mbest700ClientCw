1b6b4085002990d7d6ded1eabd92fb752f555159
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-11 09:04:22 +0300

    Update KeyboardMixin.java

--- src/main/java/com/mbest700/mixin/KeyboardMixin.java
+++ src/main/java/com/mbest700/mixin/KeyboardMixin.java
@@ -1,4 +1,5 @@
 package com.mbest700.mixin;
+
 import com.mbest700.Mbest700;
 import net.minecraft.client.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,7 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Keyboard.class)
 public class KeyboardMixin {
     @Inject(method = "onKey", at = @At("HEAD"))
-    private void onKey(long win, int key, int sc, int action, int mods, CallbackInfo ci) {
-        Mbest700.onKey(key, action);
+    private void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        // action == 1 tuşa basıldığında tetiklenir
+        if (action == 1) {
+            Mbest700.onKey(key);
+        }
     }
 }
